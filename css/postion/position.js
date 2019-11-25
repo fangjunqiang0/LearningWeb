b9ba8de8e54bda75452f2e67a5abae895ab3023c
linke <linke510420@163.com>
2019-11-25 10:44:52 +0800

    添加文件

---
+++ css/postion/position.js
@@ -0,0 +1,13 @@
+import print form './print.js'
+
+function tapClick() {
+
+    console.log("输出hello")
+    print()
+    // valueNew(212)
+}
+
+function valueNew(value) {
+
+    console.log("value == ",value)
+}
